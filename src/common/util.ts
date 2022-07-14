abe38007ecce867546ca13236420dcb5742ed7a4
Brett Settle <brettjsettle@gmail.com>
2022-07-14 00:18:38 -0700

    minor update

--- src/common/util.ts
+++ src/common/util.ts
@@ -9,19 +9,4 @@ export function getSeason() {
     }
     return 'current';
 }
-
-export const eventBus = {
-    // @ts-ignore
-    on(event, callback) {
-      document.addEventListener(event, (e) => callback(e.detail));
-    },
-    // @ts-ignore
-    dispatch(event, data) {
-      document.dispatchEvent(new CustomEvent(event, { detail: data }));
-    },
-    // @ts-ignore
-    remove(event, callback) {
-      document.removeEventListener(event, callback);
-    },
-  };
   
